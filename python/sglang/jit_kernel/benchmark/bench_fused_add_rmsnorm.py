9daaeda3a241e9fd880a036c0b7407e46f76392c
戚余航 <qiyuhang@bytedance.com>
2026-03-02 20:00:25 +0800

    Update test & benchmark.

--- python/sglang/jit_kernel/benchmark/bench_fused_add_rmsnorm.py
+++ python/sglang/jit_kernel/benchmark/bench_fused_add_rmsnorm.py
@@ -4,12 +4,70 @@ import torch
 import triton
 import triton.testing
 from flashinfer import fused_add_rmsnorm as fi_fused_add_rmsnorm
+import triton
+import triton.language as tl
 
 from sglang.jit_kernel.benchmark.utils import is_in_ci
 from sglang.jit_kernel.norm import fused_add_rmsnorm as jit_fused_add_rmsnorm
 
 IS_CI = is_in_ci()
 
+@triton.autotune(
+    configs=[
+        triton.Config({}, num_warps=1),
+        triton.Config({}, num_warps=2),
+        triton.Config({}, num_warps=4),
+        triton.Config({}, num_warps=8),
+        triton.Config({}, num_warps=16),
+        triton.Config({}, num_warps=32),
+    ],
+    key=["N"],
+    restore_value=['X', 'RES']
+)
+@triton.jit
+def rmsnorm_forward_kernel(
+    X,            # pointer to input
+    RES,          # pointer to residual
+    W,            # weight (gamma)
+    stride_x_row,
+    N,            # num cols
+    eps,
+    BLOCK_N: tl.constexpr
+):
+    row = tl.program_id(0)
+    cols = tl.arange(0, BLOCK_N)
+    mask = cols < N
+
+    x = tl.load(X + row * stride_x_row + cols, mask=mask, other=0.0).to(tl.float32)
+    res = tl.load(RES + row * stride_x_row + cols, mask=mask, other=0.0).to(tl.float32)
+    x += res
+    tl.store(RES + row * stride_x_row + cols, x, mask=mask)
+
+    var = tl.sum(x * x, axis=0) / N
+    rstd = 1 / tl.sqrt(var + eps)
+
+    x_hat = x * rstd
+    w = tl.load(W + cols, mask=mask).to(tl.float32)
+    y = x_hat * w
+
+    tl.store(X + row * stride_x_row + cols, y, mask=mask)
+
+def triton_jit_fused_add_rmsnorm(
+    input: torch.Tensor, residual: torch.Tensor, weight: torch.Tensor, eps: float
+) -> None:
+    num_tokens, dim = input.shape
+    stride_x_row = dim
+    MAX_FUSED_SIZE = 65536 // input.element_size()
+    BLOCK_N = min(MAX_FUSED_SIZE, triton.next_power_of_2(dim))
+    if dim > BLOCK_N:
+        raise RuntimeError("RMSNorm feature dim >= 64KB not supported.")
+
+    grid = (num_tokens,)
+    rmsnorm_forward_kernel[grid](
+        input, residual, weight,
+        stride_x_row, N=dim, eps=eps,
+        BLOCK_N=BLOCK_N,
+    )
 
 def sglang_jit_fused_add_rmsnorm(
     input: torch.Tensor, residual: torch.Tensor, weight: torch.Tensor, eps: float
@@ -33,8 +91,8 @@ else:
     BS_LIST = [2**n for n in range(0, 14)]
     HIDDEN_SIZE_LIST = [1536, 3072, 4096, 5120, 8192]
 
-LINE_VALS = ["jit", "fi"]
-LINE_NAMES = ["SGL JIT Kernel", "FlashInfer"]
+LINE_VALS = ["jit", "fi", "triton"]
+LINE_NAMES = ["SGL JIT Kernel", "FlashInfer", "Triton"]
 STYLES = [("orange", "-"), ("blue", "--"), ("green", "-."), ("red", ":")]
 
 configs = list(itertools.product(HIDDEN_SIZE_LIST, BS_LIST))
@@ -60,10 +118,12 @@ def benchmark(hidden_size: int, batch_size: int, provider: str):
     FN_MAP = {
         "jit": sglang_jit_fused_add_rmsnorm,
         "fi": flashinfer_fused_add_rmsnorm,
+        "triton": triton_jit_fused_add_rmsnorm,
     }
     fn = lambda: FN_MAP[provider](
         input.clone(), residual.clone(), weight, torch.finfo(torch.bfloat16).eps
     )
+    fn()    # Autotune
     quantiles = [0.5, 0.2, 0.8]
     ms, min_ms, max_ms = triton.testing.do_bench_cudagraph(fn, quantiles=quantiles)  # type: ignore
     return 1000 * ms, 1000 * max_ms, 1000 * min_ms
--- python/sglang/jit_kernel/tests/test_fused_add_rmsnorm.py
+++ python/sglang/jit_kernel/tests/test_fused_add_rmsnorm.py
@@ -2,6 +2,66 @@ import itertools
 
 import pytest
 import torch
+import triton
+import triton.language as tl
+
+@triton.autotune(
+    configs=[
+        triton.Config({}, num_warps=1),
+        triton.Config({}, num_warps=2),
+        triton.Config({}, num_warps=4),
+        triton.Config({}, num_warps=8),
+        triton.Config({}, num_warps=16),
+        triton.Config({}, num_warps=32),
+    ],
+    key=["N"],
+    restore_value=['X', 'RES']
+)
+@triton.jit
+def rmsnorm_forward_kernel(
+    X,            # pointer to input
+    RES,          # pointer to residual
+    W,            # weight (gamma)
+    stride_x_row,
+    N,            # num cols
+    eps,
+    BLOCK_N: tl.constexpr
+):
+    row = tl.program_id(0)
+    cols = tl.arange(0, BLOCK_N)
+    mask = cols < N
+
+    x = tl.load(X + row * stride_x_row + cols, mask=mask, other=0.0).to(tl.float32)
+    res = tl.load(RES + row * stride_x_row + cols, mask=mask, other=0.0).to(tl.float32)
+    x += res
+    tl.store(RES + row * stride_x_row + cols, x, mask=mask)
+
+    var = tl.sum(x * x, axis=0) / N
+    rstd = 1 / tl.sqrt(var + eps)
+
+    x_hat = x * rstd
+    w = tl.load(W + cols, mask=mask).to(tl.float32)
+    y = x_hat * w
+
+    tl.store(X + row * stride_x_row + cols, y, mask=mask)
+
+
+def triton_jit_fused_add_rmsnorm(
+    input: torch.Tensor, residual: torch.Tensor, weight: torch.Tensor, eps: float
+) -> None:
+    num_tokens, dim = input.shape
+    stride_x_row = dim
+    MAX_FUSED_SIZE = 65536 // input.element_size()
+    BLOCK_N = min(MAX_FUSED_SIZE, triton.next_power_of_2(dim))
+    if dim > BLOCK_N:
+        raise RuntimeError("RMSNorm feature dim >= 64KB not supported.")
+
+    grid = (num_tokens,)
+    rmsnorm_forward_kernel[grid](
+        input, residual, weight,
+        stride_x_row, N=dim, eps=eps,
+        BLOCK_N=BLOCK_N,
+    )
 
 
 def sglang_jit_fused_add_rmsnorm(
@@ -39,17 +99,25 @@ def test_fused_add_rmsnorm(batch_size: int, hidden_size: int) -> None:
     residual_sglang = residual.clone()
     input_flashinfer = input.clone()
     residual_flashinfer = residual.clone()
+    input_triton = input.clone()
+    residual_triton = residual.clone()
     sglang_jit_fused_add_rmsnorm(
         input_sglang, residual_sglang, weight, torch.finfo(torch.bfloat16).eps
     )
     flashinfer_fused_add_rmsnorm(
         input_flashinfer, residual_flashinfer, weight, torch.finfo(torch.bfloat16).eps
     )
+    triton_jit_fused_add_rmsnorm(
+        input_triton, residual_triton, weight, torch.finfo(torch.bfloat16).eps
+    )
     torch.testing.assert_close(input_sglang, input_flashinfer, atol=1e-2, rtol=1e-2)
     torch.testing.assert_close(
         residual_sglang, residual_flashinfer, atol=1e-2, rtol=1e-2
     )
-
+    torch.testing.assert_close(input_triton, input_flashinfer, atol=1e-2, rtol=1e-2)
+    torch.testing.assert_close(
+        residual_triton, residual_flashinfer, atol=1e-2, rtol=1e-2
+    )
 
 if __name__ == "__main__":
     pytest.main([__file__])
